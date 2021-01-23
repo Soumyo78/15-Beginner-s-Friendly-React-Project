49d61e2152d8e5c30c1bed1bb80a8f4fe72e9aa1
Soumyo78 <soumyo78@yahoo.com>
2021-01-23 15:30:56 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export default [
+  {
+    id: 1,
+    name: 'Soumyo Roy',
+    age: 23,
+    image:
+      'https://scontent.fixb1-1.fna.fbcdn.net/v/t1.0-9/79860487_1462659050548584_7689867342641102848_o.jpg?_nc_cat=105&ccb=2&_nc_sid=09cbfe&_nc_ohc=zf9DEG1ZSxoAX-SbTWO&_nc_ht=scontent.fixb1-1.fna&oh=3802a01001c1e5d21ff4202e409e70ac&oe=60318AF8'
+  },
+  {
+    id: 2,
+    name: 'Niladri Sarkar',
+    age: 23,
+    image:
+      'https://scontent.fixb1-1.fna.fbcdn.net/v/t1.0-9/142194095_1844952422319243_1759373844502801642_n.jpg?_nc_cat=102&ccb=2&_nc_sid=730e14&_nc_ohc=e3-5HCATa-4AX-GwTWV&_nc_oc=AQkVQnxeSho9aHFi2K4klMe4YD-Oqmupd0e4e4ICp3w3B9sTGe_xzGhSQdWPyEKyRfs&_nc_ht=scontent.fixb1-1.fna&oh=e5dc63e06aae158c4fc62c4ee80fa252&oe=603119B9',
+  },
+  {
+    id: 3,
+    name: 'Shantanu Nath',
+    age: 22,
+    image:
+      'https://scontent.fixb1-1.fna.fbcdn.net/v/t1.0-9/126072401_710257776252552_831813044549528900_o.jpg?_nc_cat=110&ccb=2&_nc_sid=174925&_nc_ohc=adDQB4RbtIAAX_7rYvU&_nc_ht=scontent.fixb1-1.fna&oh=71feef70928efdc449583d2bc6f0d181&oe=6032D506',
+  },
+  {
+    id: 4,
+    name: 'Sounak Dutta Chowdhury',
+    age: 21,
+    image:
+      'https://scontent.fixb1-1.fna.fbcdn.net/v/t1.0-9/125557725_665056860836529_1280452456606502997_o.jpg?_nc_cat=106&ccb=2&_nc_sid=09cbfe&_nc_ohc=8WeaqX_QIAQAX--ui1o&_nc_ht=scontent.fixb1-1.fna&oh=7d06ad89d1ca89e583f0d98180ce3103&oe=6031E603',
+  },
+  {
+    id: 5,
+    name: 'Ritam Karmakar',
+    age: 17,
+    image:
+      'https://scontent.fixb1-1.fna.fbcdn.net/v/t1.0-9/71874464_162938918236822_3633412585748955136_o.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=x0FN1ggFOcgAX-GGfCW&_nc_ht=scontent.fixb1-1.fna&oh=b613de37abeacc8d2237e513ec24d522&oe=602FEECE',
+  },
+  {
+    id: 6,
+    name: 'প্রীতি চক্রবর্তী(পম)',
+    age: 27,
+    image:
+      'https://scontent.fixb1-1.fna.fbcdn.net/v/t1.0-1/s200x200/126510218_383807579500010_2554683622179860795_o.jpg?_nc_cat=102&ccb=2&_nc_sid=7206a8&_nc_ohc=3auvOIOM7B4AX8YuPpP&_nc_ht=scontent.fixb1-1.fna&tp=7&oh=82fe21e7fc66ba0416fef52078771b89&oe=6030E900',
+  }
+  
+];
